17b823e9f54024367b0867abcbc159bbb9150112
Brook <brook@Alertas Ubuntu alertas-tic@avantika.com.co>
2025-05-29 14:33:03 -0500

    se renombra carpeta config

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 from fastapi import FastAPI
 # from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
